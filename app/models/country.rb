a9e2cae86fe0ce6c5119eef2d92d70525078f2c0
Nicolas Garcia <ngarcia@codigodelsur.com>
2019-06-13 17:28:39 -0300

    Add matches scope in Match model. Also update matches function in Country model

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,5 @@
 class Country < ApplicationRecord
     def matches
-        Match.where("country_1_id = ? OR country_2_id = ?", self.id, self.id)
+        Match.matches(self.id)        
     end
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,6 @@
 class Match < ApplicationRecord
     belongs_to :country_1, :class_name => 'Country', :foreign_key => "country_1_id"
     belongs_to :country_2, :class_name => 'Country', :foreign_key => "country_2_id"
+
+    scope :matches, -> (id) { where "country_1_id = ? OR country_2_id = ?", id, id }
 end
